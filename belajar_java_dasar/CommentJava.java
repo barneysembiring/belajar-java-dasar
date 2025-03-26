2c1e9169327f6545bf41d9d9b767ddf9ad35a1c3
barneysembiring <barneysembiring08@gmail.com>
2025-03-26 20:12:50 +0700

    update

---
+++ belajar_java_dasar/CommentJava.java
@@ -0,0 +1,19 @@
+ public class CommentJava{
+
+    public static void main(String[] args){
+
+        System.out.println(sum(10, 10));
+    }
+    /**
+        * Menghitung jumlah a + b
+        * 
+        * @param a nilai a
+        * @param b nilai b
+    */
+
+    static int sum(int a, int b){
+        //jumlahkan a + b
+
+        return a + b;
+    }
+ }
